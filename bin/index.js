17f7832917dfe399a579d9d0e99a5ad451f706f1
Nobuyuki Kondo <kondo@arc-one.jp>
2021-05-19 19:12:29 +0900

    chore: Adding rollup

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/*!
+@io-arc/preset-setup-files
+Make it easy to install frequently used files.
+
+Version: 1.0.0
+License: MIT
+Documents: https://github.com/io-arc/preset-setup-files#readme
+
+Copyright (c) 2021 arc one
+*/
+"use strict";console.log("dummy");
